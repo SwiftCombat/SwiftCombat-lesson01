b117adee3cf52c56f218b17a699d3c9777f7714e
Leandro Hernández <leandro.hdez@hotmail.com>
2015-09-27 14:24:50 +0200

    Initial commit
    - Upload base code.

---
+++ lesson01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson01
+//
+//  Created by Leandro Hernandez on 27/9/15.
+//  Copyright © 2015 SwiftCombat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
